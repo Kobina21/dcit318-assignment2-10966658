33246cf5296e3b826d911b52eae065c428e4ee4b
Kobina21 <rkaforson@st.ug.edu.gh>
2024-06-17 22:57:43 +0000

    Update Inheritance and Method Overriding.AssemblyInfo.cs

--- Inheritance and Method Overriding/obj/Debug/net8.0/Inheritance and Method Overriding.AssemblyInfo.cs
+++ Inheritance and Method Overriding/obj/Debug/net8.0/Inheritance and Method Overriding.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance and Method Overriding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ecf8848ee12c32f37df49a78fc91eee07bf0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80268c9f16ff7d3573ad2a41f85fcf87a6370dfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance and Method Overriding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance and Method Overriding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
